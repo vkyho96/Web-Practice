8695f3168151a69fe3ef893502c2845519f30114
vkyho96 <vkyho96@gmail.com>
2017-11-11 05:46:26 -0800

    hover fixed, now on js instead on css

--- todo/action.js
+++ todo/action.js
@@ -1,24 +1,23 @@
 var toggle = document.getElementsByTagName('li');
  // keep resetting... why?
- 
-// for (var i = 0; i < toggle.length; i++){
-//
-// 	toggle[i].addEventListener("mouseover", function(){
-// 			this.classList = "hover-over";
-// 	});
-//
-// 	toggle[i].addEventListener("mouseout", function(){
-// 			this.classList = "default";
-// 		});
-//
-// }
 
 for (var i = 0; i < toggle.length; i++){
 
-toggle[i].addEventListener("click", function(){
-		this.classList.toggle("clicked");
-});
+	toggle[i].addEventListener("mouseover", function(){
+			this.style.color = "green";
+	});
+
+	toggle[i].addEventListener("mouseout", function(){
+			this.style.color = "black";
+		});
+
+    toggle[i].addEventListener("click", function(){
+    		this.classList.toggle("clicked");
+    });
+
 }
+
+
 
 
 //mouseover
