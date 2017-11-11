2f4c8acd9f68cb108b9219cec8368f4bda45984f
vkyho96 <vkyho96@gmail.com>
2017-11-11 05:52:00 -0800

    js minor fixes

--- todo/action.js
+++ todo/action.js
@@ -4,11 +4,11 @@ var toggle = document.getElementsByTagName('li');
 for (var i = 0; i < toggle.length; i++){
 
 	toggle[i].addEventListener("mouseover", function(){
-			this.style.color = "green";
+			this.classList.toggle("selected");
 	});
 
 	toggle[i].addEventListener("mouseout", function(){
-			this.style.color = "black";
+			this.classList.remove("selected");
 		});
 
     toggle[i].addEventListener("click", function(){
